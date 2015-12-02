433e47ae752379caa72650d89570b00489fe99cd
Jack Rimell <jackrimell123@gmail.com>
2015-12-02 13:37:53 +0000

    fixed package.json, added logging functions

--- app/main.js
+++ app/main.js
@@ -7,4 +7,25 @@
  */
 (function(app) {
 
+  // Console.log handler
+  var log = function() {
+    if(console && console.log.apply) {
+      console.log.apply(console, arguments);
+    }
+  };
+
+  // Console.error handler
+  var error = function() {
+    if(console && console.error.apply) {
+      console.error.apply(console, arguments);
+    }
+  };
+
+  // Console.debug handler
+  var debug = function() {
+    if(console && console.debug.apply) {
+      console.debug.apply(console, arguments);
+    }
+  };
+
 }(this.Rownd = this.Rownd || {}));
