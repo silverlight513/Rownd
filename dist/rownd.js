3e02cd376b33fdade6f6852edc12b33c3cb4cb55
Jack Rimell <jackrimell123@gmail.com>
2015-12-15 19:38:36 +0000

    running grunt to track dist folder

---
+++ dist/rownd.js
@@ -0,0 +1,60 @@
+/*
+ * rownd - v0.0.1 - 2015-12-15
+ * By Jack Rimell - Copyright (c) 2015 Jack Rimell;
+*/
+(function(app) {
+
+  // Create vars needed
+  var routes = {};
+
+  // Console.error handler
+  var error = function() {
+    if(console && console.error.apply) {
+      console.error.apply(console, arguments);
+    }
+  };
+
+  /*
+   * Creates a new route for the route object
+  */
+  app.createRoute = function(route) {
+    // Check if the path property exists
+    if(route.hasOwnProperty('path') === false) {
+      error('Path value is missing from a route object');
+      return false;
+    }
+
+    // If the user has a blank path assume base url
+    if(route.path === '') {
+      route.path = '/';
+    }
+
+    // Check if the name of the controller is given
+    if(route.hasOwnProperty('controller') === false) {
+      error('Controller value is missing from a route object');
+      return false;
+    } else if(route.controller.length === 0) {
+      error('Controller value is empty in a route object');
+      return false;
+    }
+
+    // Add the new route values to the route object
+    routes[route.path] = route.controller;
+  };
+
+  /*
+   * Creates a contoller object for a route to fire
+  */
+  app.createController = function() {
+
+  };
+
+  /*
+   * Predefines the Helpers object
+  */
+  app.Helpers = (function(){
+    return {};
+  });
+
+
+}(this.Rownd = this.Rownd || {}));
