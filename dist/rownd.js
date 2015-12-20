75cedcd9130b45b7f621159cba42f64470d6dcb8
Jack Rimell <jackrimell123@gmail.com>
2015-12-20 20:03:21 +0000

    added hash change function

--- dist/rownd.js
+++ dist/rownd.js
@@ -7,6 +7,8 @@
   // Create vars needed
   var routes = {};
   var accessableControllers = {};
+  var previousHash;
+  var currentHash;
 
 
   /**
@@ -15,6 +17,135 @@
   var error = function() {
     if(console && console.error.apply) {
       console.error.apply(console, arguments);
+    }
+  };
+
+  /**
+   * @param  {String} text, The string that will be searched
+   * @param  {String} character, The character to find in the string
+   * @return {Boolean}, If the character is found at the beginning of the string
+   */
+  var startsWith = function(text, character) {
+    return text.indexOf(character) === 0;
+  };
+
+  /**
+   * @param  {String} text, The string that will be searched
+   * @param  {String} character, The character to find in the string
+   * @return {Boolean}, If the character is found at the end of the string
+   */
+  var endsWith = function(text, character){
+    return text.charAt(text.length - 1) === character;
+  };
+
+  /**
+   * @param  {String} hash, The url to be checked if it's a hash or real path
+   * @return {String}, The string without the hash if it was a hash
+   */
+  var hashOrPath = function(hash) {
+    if(startsWith(hash, '/#!')){
+      hash = hash.slice(3);
+    } else if(startsWith(hash, '/#/')){
+      hash = hash.slice(2);
+    } else if(startsWith(hash, '#')){
+      hash = hash.slice(1);
+    }
+
+    if(hash.indexOf('#') > -1){
+      hash = hash.split('#')[0];
+    }
+
+    return hash;
+  };
+
+  /**
+   * @description, Gets a route object that matches the given path
+   * @param  {String} path,
+   * @return {[type]}
+   */
+  var findMatchingRoute = function(path) {
+    // Loop through the routes object and find a matching path
+    for(var key in routes) {
+      if(routes.hasOwnProperty(key)) {
+        // If they key matches the path then use that route object
+        if(JSON.stringify(key) === path) {
+          return routes[key];
+        } else {
+          error(key, path);
+          error(key === path);
+          return false;
+        }
+      }
+    }
+  };
+
+  /**
+   * @description, Gets the current pages hash
+   * @return {String}, The new hash
+   */
+  var getNewHash = function() {
+    var hash = hashOrPath(window.location.host.length > 0 ? window.location.href.split(window.location.host)[1] : window.location.href);
+
+    // Remove trailing slash
+    if(hash.length > 1 && endsWith(hash, '/')){
+      hash = hash.slice(0, -1);
+    }
+
+    // Return a non query version of the url
+    return hash.split('?')[0];
+  };
+
+  /**
+   * @description, Loops through the routes object and clears all routes to be non-active
+   */
+  var clearActiveRoutes = function() {
+    for(var key in routes) {
+      if(routes[key] && routes[key].active) {
+        routes[key].active = false;
+      }
+    }
+  };
+
+  var runController = function() {
+    // YOURE AT THIS BIT!!!!!!!!!!!
+  };
+
+
+  /**
+   * @description, The function that fires a controller depending on what the current path is
+   * @param {newPath} newPath, The new path after clicking a link or using nav buttons
+   */
+  var navChange = function(newPath) {
+
+    // Update the previously stored hash
+    previousHash = currentHash;
+
+    // Get the new hash from param or function
+    if(!newPath) {
+      currentHash = getNewHash();
+    } else {
+      currentHash = newPath.split('?')[0];
+    }
+
+    // Find the route from the routes object
+    var matchedRoute = findMatchingRoute(currentHash);
+
+    // If a matched route could be found; fire controller and set as active
+    if(matchedRoute) {
+
+      // Clear all prevously active routes
+      clearActiveRoutes();
+
+      // Set the matched route as active
+      matchedRoute.active = true;
+
+      runController(matchedRoute.controller);
+    } else {
+      // Tell the user the new paths does not match any paths in the routes object
+      error('Cannot find current route');
+
+      // Clear all the active routes
+      clearActiveRoutes();
     }
   };
 
@@ -84,7 +215,8 @@
   var initFinished = false;
 
   var initialize = function() {
-    console.log('hello');
+    console.log('Initializing Rownd');
+    navChange();
   };
 
   // Used to detect init of page for > ie8
