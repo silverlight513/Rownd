c10ec38b6d0c55302b6ce4fb93c5e54506b6edc8
Jack Rimell <jackrimell123@gmail.com>
2015-12-31 18:58:50 +0000

    added test to check for index route with assigned controller

--- tests/main-test.js
+++ tests/main-test.js
@@ -1,5 +1,30 @@
-describe("A test suite", function() {
-   beforeEach(function() { });
-   afterEach(function() { });
-   it('should fail', function() { expect(true).to.be.false; });
+/**
+ *
+ *  List of tests for the Rownd main.js file
+ *
+ */
+
+
+
+/**
+ * @description, Checks that the helpers object is empty at initialization
+ */
+describe('Initializing helpers', function() {
+  it('should return an empty Object', function() {
+    expect(JSON.stringify(Rownd.Helpers())).to.equal('{}');
+  });
+});
+
+/**
+ * @description,
+ */
+describe('creating an index route', function() {
+  it('should have a matching controller in the routes object', function() {
+    Rownd.createRoute({path: '/', controller: 'index'});
+    expect(Rownd.routes[0].controller).to.equal('index');
+  });
+
+  it('should have a matching path in the routes object', function() {
+    expect(Rownd.routes[0].path).to.equal('/');
+  });
 });
