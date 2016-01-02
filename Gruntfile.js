e3361fb7efef939546ea54a420a3d58429ed67ab
Jack Rimell <jackrimell123@gmail.com>
2016-01-02 19:19:40 +0000

    set up grunt task for less and made first draft of the hero section

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> */',
+
+    less: {
+      dev: {
+        files: {
+          "style.css" : "style.less"
+        }
+      }
+    },
+
+    watch: {
+      less: {
+        files: ['*.less'],
+        tasks: ['less']
+      }
+    }
+
+  });
+
+  // Load all of Grunt's dependencies
+  require('matchdep')
+    .filterDev('grunt-*')
+    .forEach(grunt.loadNpmTasks);
+
+  grunt.registerTask('default', [
+    'less',
+    'watch']
+  );
+
+};
