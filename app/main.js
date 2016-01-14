f382ec246b1143fb6ad2ef8632eea02642ae2a7c
Jack Rimell <jackrimell123@gmail.com>
2016-01-14 16:42:03 +0000

    updating version numbers

--- app/main.js
+++ app/main.js
@@ -424,7 +424,7 @@
     info('Initializing Rownd');
     // Need to somehow auto update number
     if(config.showVersion){
-      info('Running Rownd v0.2.2');
+      info('Running Rownd v0.2.3');
     }
 
     // Function for loading new page
